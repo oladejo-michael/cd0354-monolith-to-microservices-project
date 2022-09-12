ab4b03bf3c2459926571a5a06262cbf92741c35a
oladejo-michael <mickeyboy16@gmail.com>
2022-09-12 16:33:06 +0100

    Completed my project 3

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'ae68d42fae14a4cb4a93e0ae41977ee6-1485155540.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
