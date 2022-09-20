c074f46babaf6a5e378fa8ccb590a04eadeb0c89
oladejo-michael <mickeyboy16@gmail.com>
2022-09-20 15:53:17 +0100

    My first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udagrammikedev
+export POSTGRES_PASSWORD=1234abcd
+export POSTGRES_HOST=udagrammikedev.c9gjqoeeeukz.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagrammikedev
+export AWS_BUCKET=arn:aws:s3:::udagrammickeydev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
